faa9926db55f0f451d954becd675ea2ca9c2f916
hos <hosamaadem8@gmail.com>
2026-02-04 14:36:06 +0300

    Refactor merge function for merging intervals

--- Leetcode/Medium/Merge Intervals.py
+++ Leetcode/Medium/Merge Intervals.py
@@ -1,26 +1,13 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        sorted_intervals=sorted(intervals)
-        result=[]
-        left,right=0,1
-        result.append(sorted_intervals[left])
+        intervals.sort()
+        result = [intervals[0]]
 
-        while right<len(intervals):
-            if result[left][1]>=sorted_intervals[right][0] and result[left][1]<=sorted_intervals[right][1]:
-                temp = result.pop()
-                print(temp)
-                result.append([temp[0],sorted_intervals[right][1]])
-                right+=1
-
-                
-            elif result[left][1]>=sorted_intervals[right][0] and result[left][1]>=sorted_intervals[right][1]:
-               
-                right+=1
-
-            
+        for start,end in intervals[1:]:
+            last=result[-1][1]
+            if start<=last:
+                result[-1][1]=max(last,end)
             else:
-                result.append(sorted_intervals[right])
-                right+=1
-                left+=1
-            
+                result.append([start,end])
+        
         return result
