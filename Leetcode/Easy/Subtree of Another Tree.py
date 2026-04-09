6d7e15b489a0c927e8a68b460b4b69ed973356f9
hos <hosamaadem8@gmail.com>
2026-04-09 23:49:40 +0300

    Create Subtree of Another Tree.py

---
+++ Leetcode/Easy/Subtree of Another Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def sametree(a, b):
+            if not a and not b:
+                return True
+            if not a or not b:
+                return False
+            if a.val != b.val:
+                return False
+            
+            return sametree(a.left, b.left) and sametree(a.right, b.right)
+
+        def dfs(node):
+            if not node:
+                return False
+            
+            if sametree(node, subRoot):
+                return True
+            
+            return dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
