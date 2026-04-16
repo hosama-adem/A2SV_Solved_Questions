7e38a4f27f614f83c393adde1ddcac06ab515550
hos <hosamaadem8@gmail.com>
2026-04-16 19:40:03 +0300

    Create Non-decreasing Subsequences.py

---
+++ Leetcode/Medium/Non-decreasing Subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(index, path):
+            if len(path) >= 2:
+                res.append(path[:])
+
+            used = set()
+            for i in range(index, len(nums)):
+                if(path and nums[i] < path[-1]) or nums[i] in used:
+                    continue
+                used.add(nums[i])
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
